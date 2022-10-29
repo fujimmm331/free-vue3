6d727f61a91d7dcc421b1c59345ca8c621254a8b
fujimmm331 <nyappy15th@gmail.com>
2022-10-29 19:38:56 +0900

    ref:不要なファイルを削除

---
+++ src/views/2048.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { reactive } from 'vue';
+
+type StateType = {
+  text: string
+}
+
+const state = reactive<StateType>({
+  text: 'this page is 2048page'
+})
+</script>
+
+<template>
+  <main>
+    <div>{{ state.text }}</div>
+  </main>
+</template>
