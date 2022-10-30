f878c4ce21bf769450aeced007fabfb9430f65b1
fujimmm331 <nyappy15th@gmail.com>
2022-10-30 13:33:38 +0900

    ref:tsdocの追加

--- src/stores/2048/FieldStore.ts
+++ src/stores/2048/FieldStore.ts
@@ -11,27 +11,52 @@ type FieldStateType = {
 export const useFieldStore = defineStore("field", () => {
   const INITIAL_NUMBERS: PowerOfTwoType[] = [2, 4, 8]
 
+  /**
+   * state
+   */
   const fieldState = reactive<FieldStateType>({
     fields: [],
     points: [],
   })
 
+  /**
+   * フィールドを初期化する処理。
+   * 指定した長さの正方形を作り、nullか初期値を設定する。
+   * @param length 一辺の長さ
+   */
   const initializeFields = (length: number) => {
     for (let y = 0; y < length; y++) {
       fieldState.fields.push(makeRow(length, y))
     }
   }
 
-  const setField = (x: number, y: number, field: PowerOfTwoType) => {
-    fieldState.fields[y][x] = field
+
+  /**
+   * フィールドに数字をセットする
+   * @param point 座標
+   * @param field セットする値
+   */
+  const setField = (point: PointType, field: PowerOfTwoType) => {
+    fieldState.fields[point.y][point.x] = field
   }
 
+  /**
+   * 数値がセットされているフィールドの座標を保存する
+   * @param point 数値がセットされているフィールドの座標
+   */
   const setPoint = (point: PointType) => {
     fieldState.points.push(point)
   }
 
 
   // private method
+
+  /**
+   * 行を作成する
+   * @param length 行の長さ
+   * @param y 数値をセットするy軸の座標
+   * @returns 行
+   */
   const makeRow = (length: number, y: number) => {
     let row: Nullable<PowerOfTwoType>[] = []
     const targetIndex = getRandomNum(length)
@@ -45,6 +70,11 @@ export const useFieldStore = defineStore("field", () => {
 
     return row
   }
+
+  /**
+   * ランダムに初期値を取得する
+   * @returns 初期値の数値
+   */
   const getInitialNumber = () => INITIAL_NUMBERS[getRandomNum(INITIAL_NUMBERS.length)]
 
   return {
