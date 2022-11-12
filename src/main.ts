0447c6dc7bfbf3be1e0a785af2471a92aaca5df1
fujimmm331 <nyappy15th@gmail.com>
2022-11-12 14:12:39 +0900

    feat:vuetfiyを導入

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,27 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
-
 import App from "./App.vue";
 import router from "./router";
-
 import "./assets/main.css";
 
-const app = createApp(App);
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 
-app.use(createPinia());
-app.use(router);
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
+const app = createApp(App);
+const libraries = [
+  createPinia(),
+  router,
+  vuetify
+]
+
+libraries.forEach(library => app.use(library))
 
 app.mount("#app");
